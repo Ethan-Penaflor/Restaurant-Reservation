6e8283844d29deca35608e687b2d06e1e39f8647
Ethan Penaflor <ethanpenaflor20@gmail.com>
2024-09-23 11:30:22 -0500

    adding frontend url

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,9 +1,8 @@
 const path = require("path");
-
 require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
 const express = require("express");
-const cors = require("cors");
+const cors = require("cors"); // Import CORS
 
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
@@ -12,7 +11,12 @@ const tablesRouter = require("../src/tables/tables.router");
 
 const app = express();
 
-app.use(cors());
+// Enable CORS for all routes
+app.use(cors({
+  origin: "https://restaurant-reservation-vgz5.onrender.com", // Specify your frontend URL
+  optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+}));
+
 app.use(express.json());
 
 app.use("/reservations", reservationsRouter);
